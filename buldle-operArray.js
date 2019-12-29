172560ecd35343bae6170ebb750860d3ea85d114
Hallan Christian <hallancma@gmail.com>
2019-12-29 14:18:56 -0300

    Adiconando arquivos operArray.html e operArray.js

---
+++ buldle-operArray.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var arr = [1, 2, 3, 4, 5, 8, 9];
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter);
+var find = arr.find(function (item) {
+  return item === 4;
+});
+console.log(find);
