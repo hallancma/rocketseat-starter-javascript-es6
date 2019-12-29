9dd36b930796d195ff51ae9c6ff3e68c421cef50
Hallan Christian <hallancma@gmail.com>
2019-12-29 15:20:00 -0300

    Adicionando arquivos objShortSyntax.html e objShortSyntax.js

---
+++ bundle-objShortSyntax.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var nome = 'Hallan';
+var idade = 23;
+var usuario = {
+  nome: nome,
+  idade: idade,
+  empresa: 'HCMA'
+};
+console.log(nome);
+console.log(idade);
+console.log(usuario);
